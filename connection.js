babffccfda5070cf25477f89828e1f9bd9fb4abf
rituraj735 <rituraj735@gmail.com>
2020-12-15 00:06:43 +0530

    pool established

--- connection.js
+++ connection.js
@@ -2,17 +2,18 @@
 var mysql = require('mysql');
 
 
-var conn = mysql.createConnection({
+var pool = mysql.createPool({
+  connectionLimit:4,
   host: "bc4wleyqxkeakwel6nqm-mysql.services.clever-cloud.com",
   user: "ukwedoierr9smgmi",
   password: "44m52UgbR3YqlIiEa4Wr",
   database:"bc4wleyqxkeakwel6nqm"
 });
 
-conn.connect((err)=> {
+pool.getConnection((err,connection)=> {
   if(err)
   throw err;
   console.log('Database connected successfully');
 });
 
-module.exports = conn;
+module.exports = pool;
