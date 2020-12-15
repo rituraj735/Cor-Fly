918e3ae3f61c83a5bae41dd33a03c262b3d90e8e
rituraj735 <rituraj735@gmail.com>
2020-12-15 16:13:38 +0530

    cancel functionality updated

--- public/cancel.js
+++ public/cancel.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   console.log("cancel js is here");
-  const button= document.getElementById('cancelbtn');
+  const button= document.getElementById('canceltn');
   button.addEventListener('click', function(e){
     console.log('button was clicked');
     fetch('/cancel', {method: 'POST'})
       .then(function(response){
         if(response.ok){
-          
+
           console.log('click recorded');
           return;
         }
